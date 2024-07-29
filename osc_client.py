6b92a4c3023b9bcd2a0c39cfbc52b8f1fc3a8c4d
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-07-28 21:10:47 -0300

    Create osc_client.py

---
+++ osc_client.py
@@ -0,0 +1,15 @@
+from pythonosc import udp_client
+
+class OscClient:
+
+    def __init__(self, ip = "127.0.0.1", port = 7400):
+        self.ip = ip
+        self.port = port
+    
+    def connect(self):
+        self.client = udp_client.SimpleUDPClient(self.ip, self.port)
+        print(f"connection... ip: {self.ip}, port: {self.port}")
+
+    def send_message(self, atr, message):
+        self.client.send_message(atr, message)
+        print(f"sending... {atr}/{message}")
