4926954482bc8c0a7de6c86e8c7d54526edfe70c
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-09-05 14:34:52 -0300

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,25 +1,26 @@
 import cv2
 import mediapipe as mp
 
-face_mesh = None
-face_detected = False
+class FaceDetection:
+    def __init__(self):
+        self.face_mesh = None
+        self.face_detected = False
+        self.cap = None
 
-def config_camera(screen_width, screen_height):
-    global cap, face_mesh
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, screen_width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, screen_height)
-    mp_face_mesh = mp.solutions.face_mesh
-    face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, min_detection_confidence=0.5)
+    def config_camera(self, screen_width, screen_height):
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, screen_width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, screen_height)
+        mp_face_mesh = mp.solutions.face_mesh
+        self.face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, min_detection_confidence=0.5)
 
-def process_face_detection():
-    global cap, face_mesh, face_detected
-    ret, camera_image = cap.read()
+    def process_face_detection(self):
+        ret, camera_image = self.cap.read()
 
-    results = face_mesh.process(camera_image)
-    
-    if results.multi_face_landmarks:
-        return True
-    else:
-        return False
+        results = self.face_mesh.process(camera_image)
         
+        if results.multi_face_landmarks:
+            return True
+        else:
+            return False
+            
